ec9a2c814bfbf8eb5786162aa8cb28031e3a2cee
LisJorge <jr17003@ues.edu.sv>
2020-07-07 18:06:17 -0600

    redireccionamiento de pagina inicial
    Ya no accede a la pantalla de welcome, pasa directamente el inicio de la aplicacion o al login sino esta logeado aun

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+Route::get('/', 'HomeController@index')->name('home')->middleware('auth');
 
 Route::group(['middleware' => 'auth'], function () {
 		Route::get('icons', ['as' => 'pages.icons', 'uses' => 'PageController@icons']);
