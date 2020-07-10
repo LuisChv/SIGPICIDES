9ddc143ea412ddbfa76e4b0e4304760ba60e249d
LuisChvz <luischavez2722@gmail.com>
2020-07-09 18:36:07 -0600

    sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -10,71 +10,100 @@
         <ul class="nav">
             <li @if ($pageSlug == 'dashboard') class="active " @endif>
                 <a href="{{ route('home') }}">
-                    <i class="tim-icons icon-chart-pie-36"></i>
-                    <p>{{ __('Dashboard') }}</p>
+                    <i class="tim-icons icon-molecule-40"></i>
+                    <p>{{ __('Mis proyectos') }}</p>
                 </a>
             </li>
             <li>
-                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
-                    <i class="fab fa-laravel" ></i>
-                    <span class="nav-link-text" >{{ __('Laravel Examples') }}</span>
+                <a data-toggle="collapse" href="#misSolicitudes" aria-expanded="false">
+                    <i class="tim-icons icon-shape-star" ></i>
+                    <span class="nav-link-text" >{{ __('Mis solicitudes') }}</span>
                     <b class="caret mt-1"></b>
                 </a>
 
-                <div class="collapse show" id="laravel-examples">
+                <div class="collapse" id="misSolicitudes">
                     <ul class="nav pl-4">
                         <li @if ($pageSlug == 'profile') class="active " @endif>
                             <a href="{{ route('profile.edit')  }}">
-                                <i class="tim-icons icon-single-02"></i>
-                                <p>{{ __('User Profile') }}</p>
+                                <i class="tim-icons icon-send"></i>
+                                <p>{{ __('Enviar solicitud') }}</p>
                             </a>
                         </li>
                         <li @if ($pageSlug == 'users') class="active " @endif>
                             <a href="{{ route('user.index')  }}">
                                 <i class="tim-icons icon-bullet-list-67"></i>
-                                <p>{{ __('User Management') }}</p>
+                                <p>{{ __('Historial') }}</p>
                             </a>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li @if ($pageSlug == 'icons') class="active " @endif>
-                <a href="{{ route('pages.icons') }}">
-                    <i class="tim-icons icon-atom"></i>
-                    <p>{{ __('Icons') }}</p>
+            <li>
+                <a data-toggle="collapse" href="#solicitudes" aria-expanded="false">
+                    <i class="tim-icons icon-email-85" ></i>
+                    <span class="nav-link-text" >{{ __('Solicitudes') }}</span>
+                    <b class="caret mt-1"></b>
                 </a>
+
+                <div class="collapse" id="solicitudes">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug == 'profile') class="active " @endif>
+                            <a href="{{ route('profile.edit')  }}">
+                                <i class="tim-icons icon-bullet-list-67"></i>
+                                <p>{{ __('Consultar') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug == 'users') class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="tim-icons icon-check-2"></i>
+                                <p>{{ __('Solicitudes a evaluar') }}</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a data-toggle="collapse" href="#investigaciones" aria-expanded="false">
+                    <i class="tim-icons icon-atom" ></i>
+                    <span class="nav-link-text" >{{ __('Investigaciones') }}</span>
+                    <b class="caret mt-1"></b>
+                </a>
+
+                <div class="collapse" id="investigaciones">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug == 'profile') class="active " @endif>
+                            <a href="{{ route('profile.edit')  }}">
+                                <i class="tim-icons icon-bullet-list-67"></i>
+                                <p>{{ __('Consultar') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug == 'users') class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="tim-icons icon-tag"></i>
+                                <p>{{ __('Tipos de investigación') }}</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
             <li @if ($pageSlug == 'maps') class="active " @endif>
                 <a href="{{ route('pages.maps') }}">
-                    <i class="tim-icons icon-pin"></i>
-                    <p>{{ __('Maps') }}</p>
+                    <i class="tim-icons icon-chart-bar-32"></i>
+                    <p>{{ __('Estadisticas') }}</p>
                 </a>
             </li>
             <li @if ($pageSlug == 'notifications') class="active " @endif>
                 <a href="{{ route('pages.notifications') }}">
-                    <i class="tim-icons icon-bell-55"></i>
-                    <p>{{ __('Notifications') }}</p>
-                </a>
-            </li>
-            <li @if ($pageSlug == 'tables') class="active " @endif>
-                <a href="{{ route('pages.tables') }}">
-                    <i class="tim-icons icon-puzzle-10"></i>
-                    <p>{{ __('Table List') }}</p>
+                    <i class="tim-icons icon-single-02"></i>
+                    <p>{{ __('Usuarios') }}</p>
                 </a>
             </li>
             <li @if ($pageSlug == 'typography') class="active " @endif>
                 <a href="{{ route('pages.typography') }}">
-                    <i class="tim-icons icon-align-center"></i>
-                    <p>{{ __('Typography') }}</p>
+                    <i class="tim-icons icon-laptop"></i>
+                    <p>{{ __('Recursos') }}</p>
                 </a>
             </li>
-            <li @if ($pageSlug == 'rtl') class="active " @endif>
-                <a href="{{ route('pages.rtl') }}">
-                    <i class="tim-icons icon-world"></i>
-                    <p>{{ __('RTL Support') }}</p>
-                </a>
-            </li>
-
         </ul>
     </div>
 </div>
