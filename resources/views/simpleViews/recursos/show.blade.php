b2e992dcaf37ca359762cae4f79ecfb586f84dc7
LuisChvz <luischavez2722@gmail.com>
2020-07-13 01:24:44 -0600

    Modificación de vistas de recursos

--- resources/views/simpleViews/recursos/show.blade.php
+++ resources/views/simpleViews/recursos/show.blade.php
@@ -4,7 +4,64 @@
 @endsection
 @section('content')
 <div class="row">
-	<div class="col-12">
+    <div class="col-7">
+        <div class="card">
+            <div class="card-header ">
+                <div class="row">
+                    <div class="col-sm-8 text-left">
+                        <h2 class="card-title"><b>Recursos disponibles</b></h2>
+                    </div>
+                    <div class="col-sm-4 text-right">
+                        <a role="button" class="btn btn-primary" href="{{ route('recursos.create')  }}">
+                            <i class="tim-icons icon-simple-add"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">                
+                    <!--Dropdown para recurso-->
+                <div id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse">
+                    <div class="list-group">
+                        @foreach ($tiposrec as $tipo)
+                        <div class="list-group-item list-group-flush">
+                            <div role="tab" id="rec{{ $tipo->id }}">
+                                <a data-toggle="collapse" data-toggle="collapse" data-target="#lista{{ $tipo->id }}" aria-expanded="false" aria-controls="lista{{ $tipo->id }}">
+                                    {{ $tipo->nombre }}&nbsp;&nbsp;
+                                    <i class="tim-icons icon-minimal-down"></i>
+                                </a>
+                            </div>
+                            <div id="lista{{ $tipo->id }}" class="collapse" aria-labelledby="rec{{ $tipo->id }}" data-parent="#accordion">
+                                <table width='100%'>
+                                    @foreach($recursos as $rec)
+                                        @if($rec->id_tipo==$tipo->id)  
+                                            <tr id={{$rec->id}} onMouseOver="ResaltarFila({{$rec->id}});" onMouseOut="RestablecerFila({{$rec->id}}, '')" onClick="CrearEnlace('{{ route('recursos.show', $rec->id)}}');">                     
+                                                <td>
+                                                </td>
+                                                <td>
+                                                    <i class="tim-icons icon-planet"></i>
+                                                    &nbsp;{{ $rec->nombre }}
+                                                </td>
+                                                <td width='10%' align="right">
+                                                    <a type="button" href="{{ route('recursos.edit', $rec->id)}}" class="btn btn-success btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-pencil"></i></a>&nbsp;&nbsp;
+                                                </td>
+                                                <td width='10%'>
+                                                    <a type="button" href="{{ route('recursos.edit', $rec->id)}}" class="btn btn-warning btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-simple-remove"></i></button>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                </table>
+                            </div>                      
+                        </div>
+                        @endforeach
+                        <!--fin de dropdown-->
+                    </div>                   
+                </div>
+            </div>
+            <div class="card-footer"><br></div>
+        </div>
+    </div>
+	<div class="col-5">
         <div class="card">
             <div class="card-header ">
                 <div class="row">
@@ -43,3 +100,17 @@
     </div>
 </div>
 @endsection
+<script langiage="javascript" type="text/javascript">
+    // RESALTAR LAS FILAS AL PASAR EL MOUSE
+    function ResaltarFila(id_fila) {
+    document.getElementById(id_fila).style.backgroundColor = '#C9EFFE';
+    }
+    // RESTABLECER EL FONDO DE LAS FILAS AL QUITAR EL FOCO
+    function RestablecerFila(id_fila, color) {
+    document.getElementById(id_fila).style.backgroundColor = color;
+    }
+    // CONVERTIR LAS FILAS EN LINKS
+    function CrearEnlace(url) {
+    location.href=url;
+    }
+</script>
