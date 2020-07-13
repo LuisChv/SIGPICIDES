0fcd7c878de9f875010a7c2c92c2e26d42207f91
LuisChvz <luischavez2722@gmail.com>
2020-07-12 19:42:45 -0600

    .

--- resources/views/simpleViews/tipoInvestigacion/listar.blade.php
+++ resources/views/simpleViews/tipoInvestigacion/listar.blade.php
@@ -4,54 +4,55 @@
 @endsection
 @section('content')
 <div class="row">
-	<div class="col-12">
+	<div class="col-6">
             <div class="card">
                 <div class="card-header ">
                     <div class="row">
-                        <div class="col-sm-8 text-left">
+                        <div class="col-sm-12 text-left">
                             <h2 class="card-title"><b>Catálogo de tipos de investigaci&oacute;n</b></h2>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
                     <!--NUEVO RECURSO-->
-                    <div class="container">
-                        <a role="button" class="btn btn-primary col-sm-2" href="{{ route('tipo_investigacion.create')  }}">
+                    <div class="container text-center">
+                        <a role="button" class="btn btn-primary col-sm-4" href="{{ route('tipo_investigacion.create')  }}">
                             <i class="tim-icons icon-simple-add"></i>&nbsp;&nbsp;Tipo
                         </a>
-                        <a role="button" class="btn btn-primary col-sm-2" href="{{ route('tipo_investigacion.create')  }}">
+                        <a role="button" class="btn btn-primary col-sm-4" href="{{ route('tipo_investigacion.create')  }}">
                             <i class="tim-icons icon-simple-add"></i>&nbsp;&nbsp;Subtipo
                         </a>
-                    </div>                    
+                    </div>
+                    <br/>                    
                     <!--Dropdown para recurso-->
                     <div id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse">
                         <div class="list-group">
                             @foreach ($tipos as $tipo)
-                            <div class="card list-group-item">
-                                <div role="tab" id="rec{{ $tipo->id }}">
-                                    <a data-toggle="collapse" data-toggle="collapse" data-target="#lista{{ $tipo->id }}" aria-expanded="false" aria-controls="lista{{ $tipo->id }}">
-                                        {{ $tipo->nombre }}&nbsp;&nbsp;
-                                        <i class="tim-icons icon-minimal-down"></i>
-                                    </a>
-                                </div>
-                                <div id="lista{{ $tipo->id }}" class="collapse" aria-labelledby="rec{{ $tipo->id }}" data-parent="#accordion">
-                                    <div class="list-group">
-                                    @foreach($sub_tipos as $sub_tipo)
-                                        @if($sub_tipo->id_tipo==$tipo->id)                        
-                                            <div class="list-group-item col-sm-7">
-                                                <i class="tim-icons icon-planet"></i>
-                                                <a href="#">&nbsp;{{ $sub_tipo->nombre }}</a>
-                                                <div class="float-right">
-                                                    <button type="button" class="btn btn-success btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-pencil"></i></button>
-                                                    <button type="button" class="btn btn-warning btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-simple-remove"></i></button>
-                                                </div>
-                                            </div>
-                                        @endif
-                                    @endforeach
+                                <div class="card list-group-item">
+                                    <div role="tab" id="rec{{ $tipo->id }}">
+                                        <a data-toggle="collapse" data-toggle="collapse" data-target="#lista{{ $tipo->id }}" aria-expanded="false" aria-controls="lista{{ $tipo->id }}">
+                                            {{ $tipo->nombre }}&nbsp;&nbsp;
+                                            <i class="tim-icons icon-minimal-down"></i>
+                                        </a>
                                     </div>
-                                </div>                      
-                            </div>
-                        @endforeach                   
+                                    <div id="lista{{ $tipo->id }}" class="collapse" aria-labelledby="rec{{ $tipo->id }}" data-parent="#accordion">
+                                        <div class="list-group">
+                                            @foreach($sub_tipos as $sub_tipo)
+                                                @if($sub_tipo->id_tipo==$tipo->id)                        
+                                                    <div class="list-group-item col-sm-12">
+                                                        <i class="tim-icons icon-planet"></i>
+                                                        <a href="#">&nbsp;{{ $sub_tipo->nombre }}</a>
+                                                        <div class="float-right">
+                                                            <button type="button" class="btn btn-success btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-pencil"></i></button>
+                                                            <button type="button" class="btn btn-warning btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-simple-remove"></i></button>
+                                                        </div>
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        </div>
+                                    </div>                      
+                                </div>
+                            @endforeach                   
                         </div>
                     </div>                   
                 </div>
