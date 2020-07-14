6b3ea09a1cd49fadfebd65f20a7e54c4a7228b06
LisJorge <jr17003@ues.edu.sv>
2020-07-14 02:06:32 -0600

    eliminar recurso arreglado

--- resources/views/simpleViews/recursos/listar.blade.php
+++ resources/views/simpleViews/recursos/listar.blade.php
@@ -43,11 +43,11 @@
                                                 <td width='10%' align="right">
                                                     <a type="button" href="{{ route('recursos.edit', $rec->id)}}" class="btn btn-success btn-sm btn-sm btn-icon btn-round"><i class="tim-icons icon-pencil"></i></a>
                                                 </td>
-                                                <form method="POST" id="formulario">
+                                                <form method="POST" id="formulario{{$rec->id}}" action="{{route('recursos.destroy', $rec->id)}}" >
                                                 @csrf
                                                 @method('DELETE')
                                                 <td width='10%'>
-                                                    <button type="button" onClick="confirmar()" class="btn btn-warning btn-sm btn-icon btn-round confirmar"><i class="tim-icons icon-simple-remove"></i></button> 
+                                                    <button type="button" onClick="confirmar({{$rec->id}})" class="btn btn-warning btn-sm btn-icon btn-round confirmar"><i class="tim-icons icon-simple-remove"></i></button> 
                                                 </td></form>
                                             </tr>
                                         @endif
@@ -80,7 +80,8 @@
     location.href=url;
     }
     require("sweetalert");
-    function confirmar(variable){
+    function confirmar(valor){
+        //ruta.concat(variable,")}}");
         swal({
           title: "¿Eliminar registro?",
           text: "Esta acción es irreversible.",
@@ -93,8 +94,7 @@
             swal("Registro eliminado", {
               icon: "success",
             });
-            document.getElementById("formulario").action="{{ route('recursos.destroy',"+variable+")}}";
-            document.getElementById("formulario").submit();
+            document.getElementById("formulario"+valor).submit();
           } else {
             swal("Eliminación cancelada");
           }
