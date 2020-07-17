0565cfe43e7dc7b951a32125b65f4e2812cde8e3
LuisChvz <luischavez2722@gmail.com>
2020-07-17 14:59:03 -0600

    vista para recuperar contraseña

---
+++ resources/views/Mail/password.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h2>Hola {{$name}}!</h2>
+    <p>A continuación te ayudaremos a recuperar tu contraseña</p>
+    <p>Para ello simplemente debes hacer click en el siguiente enlace:</p>
+    <a href="#">
+        Clic para recuperar tu contraseña
+    </a>
+    <br>
+    <p>Si no has solicitado la recuperación, ignora este correo.</p>
+</body>
+</html>
