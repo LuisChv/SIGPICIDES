ad2bb6f05c3c70694120eedac6d6263b785dfa84
Concepción <og17007@ues.edu.sv>
2020-09-15 22:23:43 -0600

    La barra lateral cuando hay mas contenido en los bloques de la tarea

--- resources/views/proyectoViews/tareas/gantt.blade.php
+++ resources/views/proyectoViews/tareas/gantt.blade.php
@@ -28,7 +28,10 @@
             height:100%;
             padding:0px;
             margin:0px;
-            overflow: hidden;
+            overflow: hidden;            
+        }
+            .gantt_cal_ltext{
+            overflow: auto;
         }
     </style>
     
@@ -64,6 +67,43 @@
         return true;
     });
     
+    gantt.config.lightbox.sections=[
+        {name:"description", height:70, map_to:"text", type:"textarea", focus:true},
+        {name:"time", height:40, map_to:"auto", type:"duration"},
+        
+        //Para mostrar el porcentaje de avance y a quienes se les asigno la tarea
+        //{name:"template", height:16, type:"template", map_to:"my_template"}, 
+    
+        /*Para asignarle un color a la tarea
+        {default_value:"#32CD32", name: "taskColor", height: 22, map_to: "color", type: "select", options:colors},  
+        Para ingresar el avance textual de la tarea
+        {name:"avance", height:70, map_to:"avance", type:"textarea"},*/
+
+        //Para usar checkbox https://docs.dhtmlx.com/gantt/desktop__checkbox.html
+        {name: "indicador", type:"checkbox", height:150, map_to: "render", options:[                
+            @php
+                    for ($i = 0; $i < sizeof($indicadores); $i++)
+                    echo '{key:"op'. $i .'", label:"' . $indicadores[$i]->detalle . '&nbsp;&nbsp; "},'
+            @endphp
+        ]},
+        //Para agrega a los miembros del equipo
+        {name: "equipo", type:"checkbox", height:60, map_to: "render", options:[    
+            @php
+                for ($i = 0; $i < sizeof($miembrosEquipo); $i++)
+                    echo '{key:"op'. $i .'", label:"' . $miembrosEquipo[$i]->name . '&nbsp;&nbsp; "},' 
+                    
+            @endphp
+        ]},
+    ];
+     //Inicializa el gant
+    gantt.init("gantt_here");
+    //Llamar al controlador para llenar los datos, aca paso por parametro el id del proyecto seleccionado
+    gantt.load("/api/data/{{$idProyecto}}");
+    //Sirve para habilitar guardar informacion en la base
+    var dp = new gantt.dataProcessor("/api");
+    dp.init(gantt);
+    dp.setTransactionMode("REST");
+    
     /*https://docs.dhtmlx.com/gantt/snippet/1c3e1c28
     Para que le asigne color a la tarea en específico
     gantt.locale.labels.section_taskColor = "Color";
@@ -92,35 +132,6 @@
     return true;
 });*/
 
-    gantt.config.lightbox.sections=[
-        {name:"description", height:70, map_to:"text", type:"textarea", focus:true},
-        {name:"time", height:40, map_to:"auto", type:"duration"},
-        
-        //Para mostrar el porcentaje de avance y a quienes se les asigno la tarea
-        //{name:"template", height:16, type:"template", map_to:"my_template"}, 
-    
-        /*Para asignarle un color a la tarea
-        {default_value:"#32CD32", name: "taskColor", height: 22, map_to: "color", type: "select", options:colors},  
-        Para ingresar el avance textual de la tarea
-        {name:"avance", height:70, map_to:"avance", type:"textarea"},*/
-
-        //Para usar checkbox https://docs.dhtmlx.com/gantt/desktop__checkbox.html
-        {name: "indicador", type:"checkbox", height:40, map_to: "render", options:[                
-            @php
-                    for ($i = 0; $i < sizeof($indicadores); $i++)
-                    echo '{key:"op'. $i .'", label:"' . $indicadores[$i]->detalle . '&nbsp;&nbsp; "},'
-            @endphp
-        ]},
-        //Para agrega a los miembros del equipo
-        {name: "equipo", type:"checkbox", height:40, map_to: "render", options:[    
-            @php
-                for ($i = 0; $i < sizeof($miembrosEquipo); $i++)
-                    echo '{key:"op'. $i .'", label:"' . $miembrosEquipo[$i]->name . '&nbsp;&nbsp; "},' 
-                    
-            @endphp
-        ]},
-    ];
-    
     /*las siguientes 3 lineas son para que la escala del gantt sea en meses, por defecto es de días, o se puede poner en años con "year" o semanas "week" en la 1ra y "%Y" o "%w" en la 3ra linea
     gantt.config.scale_unit = "month"; //display by year
     gantt.config.step = 1; //Set the step size of the time scale (X axis)
@@ -128,14 +139,7 @@
     
     //para agregar, modificar y eliminar tareas https://docs.dhtmlx.com/gantt/desktop__custom_edit_form.html
     
-    //Inicializa el gant
-    gantt.init("gantt_here");
-    //Llamar al controlador para llenar los datos, aca paso por parametro el id del proyecto seleccionado
-    gantt.load("/api/data/{{$idProyecto}}");
-    //Sirve para habilitar guardar informacion en la base
-    var dp = new gantt.dataProcessor("/api");
-    dp.init(gantt);
-    dp.setTransactionMode("REST");
+   
 </script>
 
 </body>
