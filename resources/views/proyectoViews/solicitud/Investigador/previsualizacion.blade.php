3666ae150e0a7ab7f9f6317195ba4a5ee61354ea
LuisChvz <luischavez2722@gmail.com>
2020-09-17 00:51:55 -0600

    Previsualicación de recursos

--- resources/views/proyectoViews/solicitud/Investigador/previsualizacion.blade.php
+++ resources/views/proyectoViews/solicitud/Investigador/previsualizacion.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app',['pageSlug' => 'dashboard'])
 @section('title')
-	Asignar recursos
+Asignar recursos
 @endsection
 @section('content')
 <div class="row">
     <div class="col-md-12">
         <div class="card">
-                
+            
             <div class="card-body">
                 <h3>
                     Información general
@@ -75,12 +75,12 @@
             <div class="card-body">
                 <table class="table">
                     @foreach ($objetivos as $objetivo)
-                        <tr>
-                            <td class="align-text-top"><i class="tim-icons icon-compass-05"></i></td>
-                            <td>
-                                {{$objetivo->descripcion}}
-                            </td>
-                        </tr>
+                    <tr>
+                        <td class="align-text-top"><i class="tim-icons icon-compass-05"></i></td>
+                        <td>
+                            {{$objetivo->descripcion}}
+                        </td>
+                    </tr>
                     @endforeach
                 </table>
             </div>
@@ -96,11 +96,11 @@
             <div class="card-body">
                 <table class="table">
                     @foreach ($alcances as $alcance)
-                        <tr>
-                            <td>
-                                <i class="tim-icons icon-compass-05"></i> {{$alcance->descripcion}}
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>
+                            <i class="tim-icons icon-compass-05"></i> {{$alcance->descripcion}}
+                        </td>
+                    </tr>
                     @endforeach
                 </table>
             </div>
@@ -116,11 +116,11 @@
             <div class="card-body">
                 <table class="table">
                     @foreach ($indicadores as $indicador)
-                        <tr>
-                            <td>
-                                <i class="tim-icons icon-sound-wave"></i> {{$indicador->detalle}}
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>
+                            <i class="tim-icons icon-sound-wave"></i> {{$indicador->detalle}}
+                        </td>
+                    </tr>
                     @endforeach
                 </table>
             </div>
@@ -133,49 +133,30 @@
             </div>
             <div class="card-body">
                 <div class="card-body">  
-                    <div id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse">
-                        <div class="list-group">
-                            @foreach ($tiposrec as $tipo)
-                                <div>
-                                    <table width='100%'>
-                                        <tr class="list-group-item list-group-flush" data-toggle="collapse" data-toggle="collapse" data-target="#lista{{ $tipo->id }}" aria-expanded="false" aria-controls="lista{{ $tipo->id }}">
-                                            <td>
-                                                {{ $tipo->nombre }}&nbsp;&nbsp;
-                                                <i class="tim-icons icon-minimal-down"></i>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                    <div id="lista{{ $tipo->id }}" class="collapse" aria-labelledby="rec{{ $tipo->id }}" data-parent="#accordion">
-                                        <table width='100%' class="table">
-                                            <tr>
-                                                <th>Recursos</th>
-                                                <th>Detalle</th>
-                                                <th class="text-center">Cantidad</th>
-                                            </tr>
-                                            @foreach($recursosProy as $rec)
-                                                @if($rec->id_tipo==$tipo->id)  
-                                                    <tr>                     
-                                                        <td>
-                                                            &nbsp;&nbsp;&nbsp;&nbsp;<i class="tim-icons icon-planet"></i>
-                                                            &nbsp;{{ $rec->nombre }}
-                                                        </td> 
-                                                        <td>
-                                                            {{ $rec->detalle }}
-                                                        </td>    
-                                                        <td class="text-center">
-                                                            {{ $rec->cantidad }}
-                                                        </td>                            
-                                                    </tr>
-                                                @endif
-                                            @endforeach
-                                        </table>
-                                    </div>                      
-                                </div>
-                            @endforeach
-                            <!--fin de dropdown-->
-                            </br>
-                        </div>                   
-                    </div>
+                    
+                    <table width='100%' class="table">
+                        <tr>
+                            <th width='30%'>Recursos</th>
+                            <th width='30%'>Cantidad</th>
+                            <th width='40%'>Detalle</th>
+                        </tr>
+                        @foreach($recursosProy as $rec)
+                        <tr>                     
+                            <td>
+                                &nbsp;&nbsp;&nbsp;&nbsp;<i class="tim-icons icon-planet"></i>
+                                &nbsp;{{ $rec->nombre }}
+                            </td> 
+                            <td>
+                                {{ $rec->cantidad }}
+                            </td>  
+                            <td>
+                                {{ $rec->detalle }}
+                            </td>                            
+                        </tr>
+                        @endforeach
+                    </table>
+                    
+                    <br>
                 </div>
             </div>
         </div> 
@@ -188,16 +169,16 @@
                 Recursos
             </a>
         </td>
-
+        
         <td width="50%" align="right">
             @if ($solicitud->id_estado == 2)
-                <a class="btn btn-primary" href="{{ route('solicitud.enviar', [$proyecto->id]) }}">
-                    Enviar
-                </a>
+            <a class="btn btn-primary" href="{{ route('solicitud.enviar', [$proyecto->id]) }}">
+                Enviar
+            </a>
             @else
-                <a class="btn btn-primary" href="{{ route('solicitud.mis_solicitudes') }}">
-                    Mis solicitudes
-                </a>
+            <a class="btn btn-primary" href="{{ route('solicitud.mis_solicitudes') }}">
+                Mis solicitudes
+            </a>
             @endif
         </td>
     </tr>
