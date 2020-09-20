afd803815918483d2f508ae4130d96864e03a742
LuisChvz <luischavez2722@gmail.com>
2020-09-19 20:51:00 -0600

    migraciones

--- database/migrations/2020_05_15_000003_create_proyectos_table.php
+++ database/migrations/2020_05_15_000003_create_proyectos_table.php
@@ -17,20 +17,20 @@ class CreateProyectosTable extends Migration
             $table->id();
             $table->integer('id_subtipo');
             $table->integer('id_equipo');
-            $table->string('nombre', 256);
+            $table->string('nombre', 1024);
             $table->integer('id_comite')->nullable();
             $table->integer('duracion')->nullable();
             $table->boolean('modificable')->default(true);
-            $table->string('descripcion', 512);
+            $table->string('descripcion', 1024);
             $table->decimal('costo', 7)->nullable();
             $table->integer('id_estado')->nullable();
             $table->foreign('id_estado')->references('id')->on('estado_de_proy')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('id_subtipo')->references('id')->on('subtipo_de_investigacion')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('id_equipo')->references('id')->on('equipo_de_investigacion')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('id_comite')->references('id')->on('comite_de_evaluacion')->onUpdate('cascade')->onDelete('cascade');
-            $table->string('tema', 256);
-            $table->string('resultados', 256);
-            $table->string('justificacion', 256);
+            $table->string('tema', 1024);
+            $table->string('resultados', 1024);
+            $table->string('justificacion', 1024);
             $table->timestamps();
         });
     }
