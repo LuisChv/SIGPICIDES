e778c23e8be1f696d7cbd2ed489a54041a6006c3
LisJorge <jr17003@ues.edu.sv>
2020-11-29 11:57:42 -0600

    correccion color navbar indicadores

--- resources/views/proyectoViews/indicador/show.blade.php
+++ resources/views/proyectoViews/indicador/show.blade.php
@@ -7,9 +7,9 @@
 		<div class="col-12">
 			<nav>
 				<div class="nav nav-tabs" id="nav-tab" role="tablist">
-				    <a href="{{route('indicador.general', $indicador->id)}}" style="color:primary; !important font-style: bold; !important" @if($pageSlug == 'general') class="nav-item nav-link active" @else class="nav-item nav-link" @endif>General</a>
-				    <a href="{{route('indicador.estadistica', $indicador->id)}}" style="color:primary; !important font-style: bold; !important" @if($pageSlug == 'estadistica') class="nav-item nav-link active" @else class="nav-item nav-link" @endif>Estadísticas</a>
-				    <a href="{{route('indicador.task', $indicador->id)}}" style="color:primary; !important font-style: bold; !important" @if($pageSlug == 'task') class="nav-item nav-link active" @else class="nav-item nav-link" @endif>Tareas</a>
+				    <a href="{{route('indicador.general', $indicador->id)}}" @if($pageSlug == 'general') class="nav-item nav-link navbarCustom" @else class="nav-item nav-link" @endif>General</a>
+				    <a href="{{route('indicador.estadistica', $indicador->id)}}" @if($pageSlug == 'estadistica') class="nav-item nav-link navbarCustom" @else class="nav-item nav-link" @endif>Estadísticas</a>
+				    <a href="{{route('indicador.task', $indicador->id)}}" @if($pageSlug == 'task') class="nav-item nav-link navbarCustom" @else class="nav-item nav-link" @endif>Tareas</a>
 				</div>
 			</nav>
 			<br>
